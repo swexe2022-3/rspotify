857625316459c59dafe410f10ea50119b8154c25
kimjunhyeok18 <1811140059a@ed.fuk.kindai.ac.jp>
2022-11-30 06:54:45 +0000

    fix: 友達リストの変更

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,15 +4,24 @@ class FriendsController < ApplicationController
         @friend = Friend.new
     end
     def create
-        if User.find_by(spotify_uid: params[:friend][:friend_uid])
-            friend = Friend.create(uid: session[:spotify_uid], friend_uid: params[:friend][:friend_uid], user_id: User.find_by(spotify_uid: session[:spotify_uid]))
-            friend.save
-            redirect_to '/friends'
-        else 
-            redirect_to '/friends',:notice => 'not found'
+        #入力したuidのユーザーと友達なのか確認
+        if Friend.find_by(uid: session[:spotify_uid], friend_uid: params[:friend][:friend_uid])
+            redirect_to '/friends',flash: {fail: 'already friend'}
+        else
+            #入力したuidのユーザーがいるか確認
+            if User.find_by(spotify_uid: params[:friend][:friend_uid])
+                Friend.create!(uid: session[:spotify_uid], friend_uid: params[:friend][:friend_uid])
+                redirect_to '/friends',flash: {success: 'Success to add'}
+            else 
+                redirect_to '/friends',flash: {fail: 'id not found'}
+            end
         end
+
     end
     def destroy
+        friend = Friend.find(params[:id])
+        friend.destroy
+        redirect_to '/friends'
     end
     def show
     end
