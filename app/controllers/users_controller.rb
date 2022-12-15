7d5d2875e0be2ef924feb7191437aaecf7947cee
kimjunhyeok18 <1811140059a@ed.fuk.kindai.ac.jp>
2022-12-15 08:19:48 +0000

    fix: トークンの受け渡しにユーザー登録するように変更、友達数が０でもformが見えたため変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
         hash = spotify_user.to_hash
         spotify_user = RSpotify::User.new(hash)
         session[:spotify_uid] = spotify_user.id
+        @user = User.find_or_create_by!(spotify_uid: session[:spotify_uid])
         redirect_to top_user_update_path
     end
     def show
