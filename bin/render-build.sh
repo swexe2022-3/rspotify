77ca52fd4bb5d7c656120fc0ecbdb3e6d40c54ef
2011140071b <kyo925kyo@gmail.com>
2023-01-15 11:45:00 +0000

    アプリ名変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,4 @@
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-#bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate
